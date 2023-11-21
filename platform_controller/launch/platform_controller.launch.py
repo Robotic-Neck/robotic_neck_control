23f590bd7f2fb39b95013a25df8f170d77bbd0c8
octavioaguila <oeaguila@uc.cl>
2023-11-21 18:13:31 -0300

    linear controller added

--- platform_controller/launch/platform_controller.launch.py
+++ platform_controller/launch/platform_controller.launch.py
@@ -5,12 +5,19 @@ from launch_utils.utils import launch_rviz_node
 
 def generate_launch_description():
 
-    # Set the parameters for the motors velocity  PID controller
-    out_min = -250
-    out_max = 250
-    kp = 1.6
-    ki = 16.4
-    kd = 0.31
+    # Set the parameters for the motors velocity PID controller
+    motor_out_min = -250
+    motor_out_max = 250
+    motors_kp = 1.6
+    motors_ki = 16.4
+    motors_kd = 0.31
+
+    # Set the parameters for the lineal actuators PID controller
+    lineal_actuator_out_min = -20
+    lineal_actuator_out_max = 20
+    lineal_actuators_kp = 1000.0
+    lineal_actuators_ki = 0.0
+    lineal_actuators_kd = 0.0
 
     return LaunchDescription([
         Node(package='robotic_neck_viz', executable='neck_state_joint_publisher', name='neck_state_joint_publisher'),
@@ -28,9 +35,9 @@ def generate_launch_description():
             name='motors_velocity_controller_right',
             output='screen',
             parameters=[
-                {'kp' : kp},
-                {'ki' : ki},
-                {'kd' : kd},
+                {'kp' : motors_kp},
+                {'ki' : motors_ki},
+                {'kd' : motors_kd},
                 {'use_sample_time' : False},
                 {'sample_time' : 2},
                 {'derivative_on_measurement' : False},
@@ -38,8 +45,8 @@ def generate_launch_description():
                 {'reset_windup' : False},
                 {'pid_enabled' : True},
                 {'cut_off_freq' : 0},
-                {'out_min' : out_min},
-                {'out_max' : out_max},
+                {'out_min' : motor_out_min},
+                {'out_max' : motor_out_max},
                 {'control_value_topic' : 'right_motor/control_effort'},
                 {'actual_state_topic' : 'right_motor/actual_state'},
                 {'set_point_topic' : 'right_motor/set_point'},
@@ -52,9 +59,9 @@ def generate_launch_description():
             name='motors_velocity_controller_left',
             output='screen',
             parameters=[
-                {'kp' : kp},
-                {'ki' : ki},
-                {'kd' : kd},
+                {'kp' : motors_kp},
+                {'ki' : motors_ki},
+                {'kd' : motors_kd},
                 {'use_sample_time' : False},
                 {'sample_time' : 2},
                 {'derivative_on_measurement' : False},
@@ -62,11 +69,59 @@ def generate_launch_description():
                 {'reset_windup' : False},
                 {'pid_enabled' : True},
                 {'cut_off_freq' : 0},
-                {'out_min' : out_min},
-                {'out_max' : out_max},
+                {'out_min' : motor_out_min},
+                {'out_max' : motor_out_max},
                 {'control_value_topic' : 'left_motor/control_effort'},
                 {'actual_state_topic' : 'left_motor/actual_state'},
                 {'set_point_topic' : 'left_motor/set_point'},
+                {'loop_freq' : 10}
+            ]
+        ),
+        Node(
+            package='motors_velocity_controller',
+            executable='motors_velocity_controller',
+            name='left_linear_actuator_controller',
+            output='screen',
+            parameters=[
+                {'kp' : lineal_actuators_kp},
+                {'ki' : lineal_actuators_ki},
+                {'kd' : lineal_actuators_kd},
+                {'use_sample_time' : False},
+                {'sample_time' : 2},
+                {'derivative_on_measurement' : False},
+                {'remove_ki_bump' : False},
+                {'reset_windup' : False},
+                {'pid_enabled' : True},
+                {'cut_off_freq' : 0},
+                {'out_min' : lineal_actuator_out_min},
+                {'out_max' : lineal_actuator_out_max},
+                {'control_value_topic' : 'left_actuator/control_effort'},
+                {'actual_state_topic' : 'left_actuator/actual_state'},
+                {'set_point_topic' : 'left_actuator/set_point'},
+                {'loop_freq' : 10}
+            ]
+        ),
+        Node(
+            package='motors_velocity_controller',
+            executable='motors_velocity_controller',
+            name='right_linear_actuator_controller',
+            output='screen',
+            parameters=[
+                {'kp' : lineal_actuators_kp},
+                {'ki' : lineal_actuators_ki},
+                {'kd' : lineal_actuators_kd},
+                {'use_sample_time' : False},
+                {'sample_time' : 2},
+                {'derivative_on_measurement' : False},
+                {'remove_ki_bump' : False},
+                {'reset_windup' : False},
+                {'pid_enabled' : True},
+                {'cut_off_freq' : 0},
+                {'out_min' : lineal_actuator_out_min},
+                {'out_max' : lineal_actuator_out_max},
+                {'control_value_topic' : 'right_actuator/control_effort'},
+                {'actual_state_topic' : 'right_actuator/actual_state'},
+                {'set_point_topic' : 'right_actuator/set_point'},
                 {'loop_freq' : 10}
             ]
         ),
--- platform_controller/platform_controller/platform_manager.py
+++ platform_controller/platform_controller/platform_manager.py
@@ -1,9 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.duration import Duration
 from std_msgs.msg import Float32
 from sensor_msgs.msg import JointState
-import math
+from message_filters import Subscriber, ApproximateTimeSynchronizer
 
 
 class PlatformManager(Node):
@@ -14,88 +13,70 @@ class PlatformManager(Node):
     """
     def __init__(self):
         super().__init__('platform_manager')
-        self.r_motor_velocity = Float32(data=12.56) # rad/s
-        self.l_motor_velocity = Float32(data=12.56) # rad/s
 
-        self.prev_l_actuator_pos = 0.0
-        self.prev_r_actuator_pos = 0.0
+        self.linear_actuator_setpoint = 0.0
 
         # Create the publishers with angular velocity setpoints for each motor
         self.l_motor_vel_setpoint_pub = self.create_publisher(Float32, '/left_motor/set_point', 10)
         self.r_motor_vel_setpoint_pub = self.create_publisher(Float32, '/right_motor/set_point', 10)
 
-        # Create the subscribers to the inverse kinematics nodes
-        self.joint_states_sub = self.create_subscription(JointState, 'target/joint_states',
-                                                         self.joint_states_callback, 1)
+        # Create the publishers with the actual state lineal actuators
+        self.l_actuator_actual_state_pub = self.create_publisher(Float32, '/left_actuator/actual_state', 10)
+        self.r_actuator_actual_state_pub = self.create_publisher(Float32, '/right_actuator/actual_state', 10)
 
-    def joint_states_callback(self, msg):
-        actual_l_actuator_pos = msg.position[9]
-        actual_r_actuator_pos = msg.position[4]
+        # Create the subscribers with the velocity control effort for lineal actuators
+        self.l_actuator_control_effort_sub = self.create_subscription(Float32, '/left_actuator/control_effort',
+                                                                       self.l_actuator_control_effort_callback, 10)
+        self.r_actuator_control_effort_sub = self.create_subscription(Float32, '/right_actuator/control_effort',
+                                                                       self.r_actuator_control_effort_callback, 10)
+
+        # Create the subscribers to the inverse kinematics nodes
+        self.target_joint_states_sub = Subscriber(self, JointState, 'target/joint_states')
+        self.state_joint_states_sub = Subscriber(self, JointState, 'state/joint_states')
+        
+        # Create the synchronizer with a 0.1 seconds tolerance
+        self.syncronizer = ApproximateTimeSynchronizer([self.target_joint_states_sub, self.state_joint_states_sub], 10, 0.1)
+        self.syncronizer.registerCallback(self.joint_states_callback)
+
+        self.get_logger().info('Platform manager node started')
+
+    def joint_states_callback(self, target_joint_states, actual_joint_states):
+        """Inverse kinematics callback function, it calculates the error distance to move the lineal actuators"""
+        target_l_actuator_pos = target_joint_states.position[9]
+        target_r_actuator_pos = target_joint_states.position[4]
+
+        actual_l_actuator_pos = actual_joint_states.position[9]
+        actual_r_actuator_pos = actual_joint_states.position[4]
 
         l_dist = 0.0
         r_dist = 0.0
 
-        # Lineal actuator goes down
-        if actual_l_actuator_pos < self.prev_l_actuator_pos:
-            self.l_motor_velocity.data = -12.56
-            l_dist = abs(actual_l_actuator_pos - self.prev_l_actuator_pos)
-        # Lineal actuator goes up
-        elif actual_l_actuator_pos > self.prev_l_actuator_pos:
-            self.l_motor_velocity.data = 12.56
-            l_dist = abs(actual_l_actuator_pos - self.prev_l_actuator_pos)
+        l_dist = abs(actual_l_actuator_pos - target_l_actuator_pos)
+        r_dist = abs(actual_r_actuator_pos - target_r_actuator_pos)
 
-        # Lineal actuator goes down
-        if actual_r_actuator_pos < self.prev_r_actuator_pos:
-            self.r_motor_velocity.data = -12.56
-            r_dist = abs(actual_r_actuator_pos - self.prev_r_actuator_pos)
-        elif actual_r_actuator_pos > self.prev_r_actuator_pos:
-            self.r_motor_velocity.data = 12.56
-            r_dist = abs(actual_r_actuator_pos - self.prev_r_actuator_pos)
-
-        self.prev_r_actuator_pos = actual_r_actuator_pos
-        self.prev_l_actuator_pos = actual_l_actuator_pos
-
-        self.get_logger().info(f'Left actuator distance: {l_dist}')
-        self.get_logger().info(f'Right actuator distance: {r_dist}')
-
-
-        if l_dist > 0.0:
-            l_dist = l_dist * 1000 # m to mm
-            l_rev = l_dist / 2 # mm to rev
-            l_time = abs((l_rev * 2 * math.pi) / self.l_motor_velocity.data)
-            self.l_motor_run_duration = Duration(seconds=l_time)
-            self.l_motor_start_time = self.get_clock().now()
-            self.l_motor_timer = self.create_timer(0.1, self.publish_left_motor_velocity)
-
-
-        if r_dist > 0.0:
-            r_dist = r_dist * 1000
-            r_rev = r_dist / 2
-            r_time = abs((r_rev * 2 * math.pi) / self.r_motor_velocity.data)
-            self.r_motor_run_duration = Duration(seconds=r_time)
-            self.r_motor_start_time = self.get_clock().now()
-            self.r_motor_timer = self.create_timer(0.1, self.publish_right_motor_velocity)
-
-    def publish_left_motor_velocity(self):
-        current_time = self.get_clock().now()
-        if (current_time - self.l_motor_start_time) < self.l_motor_run_duration:
-            # Todavía estamos dentro del período de tiempo, publica la velocidad
-            self.l_motor_vel_setpoint_pub.publish(self.l_motor_velocity)
-        else:
-            # El tiempo ha terminado, así que detenemos el temporizador y, por lo tanto, la publicación
-            self.l_motor_timer.cancel()
-            self.l_motor_vel_setpoint_pub.publish(Float32(data=0.0))
-
+        if actual_l_actuator_pos < target_l_actuator_pos:
+            l_dist = -l_dist
+        if actual_r_actuator_pos < target_r_actuator_pos:
+            r_dist = -r_dist
             
-    def publish_right_motor_velocity(self):
-        current_time = self.get_clock().now()
-        if (current_time - self.r_motor_start_time) < self.r_motor_run_duration:
-            # Todavía estamos dentro del período de tiempo, publica la velocidad
-            self.r_motor_vel_setpoint_pub.publish(self.r_motor_velocity)
-        else:
-            # El tiempo ha terminado, así que detenemos el temporizador y, por lo tanto, la publicación
-            self.r_motor_timer.cancel()
-            self.r_motor_vel_setpoint_pub.publish(Float32(data=0.0))
+        # self.get_logger().info(f'Left actuator distance: {l_dist}')
+        # self.get_logger().info(f'Right actuator distance: {r_dist}')
+
+        self.l_actuator_actual_state_pub.publish(Float32(data=l_dist))
+        self.r_actuator_actual_state_pub.publish(Float32(data=r_dist))
+
+
+    def l_actuator_control_effort_callback(self, msg):
+        # Limit the control effort to 20.0
+        if msg.data > 20:
+            msg.data = 20.0
+        self.l_motor_vel_setpoint_pub.publish(Float32(data=msg.data))
+            
+    def r_actuator_control_effort_callback(self, msg):
+        # Limit the control effort to 20.0
+        if msg.data > 20:
+            msg.data = 20.0
+        self.r_motor_vel_setpoint_pub.publish(Float32(data=msg.data))
 
 def main(args=None):
     rclpy.init(args=args)
